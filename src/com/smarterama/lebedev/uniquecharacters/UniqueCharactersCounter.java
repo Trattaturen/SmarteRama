d882ca2d2dba5de7a082b489c6dafa19620ff267
Dmitry Lebedev <lebedev.d.y@gmail.com>
2016-07-13 12:24:37 +0300

    Change algorithm to return all characters with its count

--- src/com/smarterama/lebedev/uniquecharacters/UniqueCharactersCounter.java
+++ src/com/smarterama/lebedev/uniquecharacters/UniqueCharactersCounter.java
@@ -1,31 +1,39 @@
 package com.smarterama.lebedev.uniquecharacters;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
+import java.util.TreeMap;
 
 public class UniqueCharactersCounter {
 
-	private Map<String, Integer> previousResults = new HashMap<String, Integer>();
+	private Map<String, Map<Character, Integer>> cachedResults = new HashMap<>();
 
-	public int countUniqueCharacters(String initialText) {
+	public Map<Character, Integer> countUniqueCharacters(String initialString) {
 
-		if (previousResults.containsKey(initialText)) {
-			System.out.println("Got from map");
-			return previousResults.get(initialText);
+		if (cachedResults.containsKey(initialString)) {
+
+			System.out.println("Got from cache");
+			return cachedResults.get(initialString);
+
 		}
 
-		System.out.println("Not in map");
-		Set<Character> uniqueCharacters = new HashSet<Character>();
+		Map<Character, Integer> currentResult = new TreeMap<>();
 
-		for (int i = 0; i < initialText.length(); i++) {
-			uniqueCharacters.add(initialText.charAt(i));
+		for (int i = 0; i < initialString.length(); i++) {
+
+			char currentChar = initialString.charAt(i);
+
+			if (currentResult.containsKey(currentChar)) {
+
+				currentResult.put(currentChar, currentResult.get(currentChar) + 1);
+			} else {
+
+				currentResult.put(currentChar, 1);
+			}
 		}
-		int result = uniqueCharacters.size();
-		previousResults.put(initialText, result);
 
-		return result;
+		cachedResults.put(initialString, currentResult);
+		return currentResult;
 
 	}
 }
--- test/com/smarterama/lebedev/uniquecharacters/UniqueCharactersCounterTest.java
+++ test/com/smarterama/lebedev/uniquecharacters/UniqueCharactersCounterTest.java
@@ -9,43 +9,45 @@ public class UniqueCharactersCounterTest {
 
 	@Test
 	public void testUniqueText1() {
-		Assert.assertEquals(5, counter.countUniqueCharacters("12345"));
+		Assert.assertEquals("{1=1, 2=1, 3=1, 4=1, 5=1}", counter.countUniqueCharacters("12345").toString());
 	}
 
 	@Test
 	public void testUniqueText2() {
-		Assert.assertEquals(23, counter.countUniqueCharacters(
-				"Java is a general-purpose computer programming language that is concurrent, class-based, object-oriented"));
+		Assert.assertEquals("{ =11, ,=2, -=3, J=1, a=10, b=2, c=5, d=2, e=10, g=5, h=1, i=4, j=1, l=3, m=3, n=6, o=6, p=4, r=8, s=6, t=6, u=4, v=1}", counter.countUniqueCharacters(
+				"Java is a general-purpose computer programming language that is concurrent, class-based, object-oriented").toString());
 	}
-
+	
 	@Test
 	public void testUniqueText3() {
-		Assert.assertEquals(10, counter.countUniqueCharacters("!@#$%^&*()"));
+		Assert.assertEquals("{!=1, #=1, $=1, %=1, &=1, (=1, )=1, *=1, @=1, ^=1}", counter.countUniqueCharacters("!@#$%^&*()").toString());
 	}
 
 	@Test
 	public void testUniqueText4() {
-		Assert.assertEquals(10, counter.countUniqueCharacters("qwertyuioppoiuytrewq"));
+		Assert.assertEquals("{e=2, i=2, o=2, p=2, q=2, r=2, t=2, u=2, w=2, y=2}", counter.countUniqueCharacters("qwertyuioppoiuytrewq").toString());
 	}
 
 	@Test
 	public void testUniqueText5() {
-		Assert.assertEquals(8, counter.countUniqueCharacters("AaBbCcDd"));
+		Assert.assertEquals("{A=1, B=1, C=1, D=1, a=1, b=1, c=1, d=1}", counter.countUniqueCharacters("AaBbCcDd").toString());
 	}
 
 	@Test
 	public void testUniqueText6() {
-		Assert.assertEquals(1, counter.countUniqueCharacters("                  "));
+		Assert.assertEquals("{ =18}", counter.countUniqueCharacters("                  ").toString());
 	}
 
 	@Test
 	public void testUniqueText7() {
-		Assert.assertEquals(0, counter.countUniqueCharacters(""));
+		Assert.assertEquals("{}", counter.countUniqueCharacters("").toString());
 	}
 
 	@Test
 	public void testGotFromMap() {
-		Assert.assertEquals(3, counter.countUniqueCharacters("sym"));
-		Assert.assertEquals(3, counter.countUniqueCharacters("sym"));
+		Assert.assertEquals("{m=1, s=1, y=1}", counter.countUniqueCharacters("sym").toString());
+		Assert.assertEquals("{m=1, s=1, y=1}", counter.countUniqueCharacters("sym").toString());
 	}
 }
+
+
